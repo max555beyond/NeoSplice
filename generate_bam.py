b5003108f113f2aa49193304cb82a96a1e70e0aa
max555beyond <shengjie@email.unc.edu>
2019-05-09 00:31:46 -0400

    added code for corner case

--- generate_bam.py
+++ generate_bam.py
@@ -65,79 +65,108 @@ class Trie():
         return self._pattern(self.dump())
 
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s [%(processName)s.%(levelname)s] %(message)s")
-parser = argparse.ArgumentParser(description="Utility for retrieving tumor specific kmers in RNA-seq reads.")
-parser.add_argument("Kmer_file", type=str, nargs='?', help="provide Kmer file here")
-parser.add_argument("input_bam_file", type=str, nargs='?', help="provide input bam file path here")
-parser.add_argument("out_bam_file", type=str, nargs='?', help="provide output bam file path here")
-args = parser.parse_args()
+def main():
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s [%(processName)s.%(levelname)s] %(message)s")
+    parser = argparse.ArgumentParser(description="Utility for retrieving tumor specific kmers in RNA-seq reads.")
+    parser.add_argument("Kmer_file", type=str, nargs='?', help="provide Kmer file here")
+    parser.add_argument("input_bam_file", type=str, nargs='?', help="provide input bam file path here")
+    parser.add_argument("out_bam_file", type=str, nargs='?', help="provide output bam file path here")
+    args = parser.parse_args()
 
-cigar_map = {0:'M', 1:'I', 2:'D', 3:'N', 4:'S', 5:'H', 6:'P', 7:'=', 8:'X', 9:'B'}
+    cigar_map = {0:'M', 1:'I', 2:'D', 3:'N', 4:'S', 5:'H', 6:'P', 7:'=', 8:'X', 9:'B'}
 
-samfile = pysam.AlignmentFile(args.input_bam_file, "rb")
-kmer_reads = pysam.AlignmentFile(args.out_bam_file, "wb", template=samfile)
+    samfile = pysam.AlignmentFile(args.input_bam_file, "rb")
+    kmer_reads = pysam.AlignmentFile(args.out_bam_file, "wb", template=samfile)
 
-trie = Trie()
+    trie = Trie()
 
-with open(args.Kmer_file) as f:
-    for line in f:
-        line_split = line.strip().split('\t')
-        trie.add(line_split[0])
+    with open(args.Kmer_file) as f:
+        for line in f:
+            line_split = line.strip().split('\t')
+            trie.add(line_split[0])
 
-pattern = re.compile(trie.pattern())
+    pattern = re.compile(trie.pattern())
+    print trie.pattern()
+    for read in samfile.fetch():
+        if not read.is_unmapped and not read.is_secondary and read.is_proper_pair and read.is_paired and\
+                not read.is_duplicate and not read.is_supplementary and 'N' in read.cigarstring:
 
-for read in samfile.fetch():
-    if not read.is_unmapped and not read.is_secondary and read.is_proper_pair and read.is_paired and\
-            not read.is_duplicate and not read.is_supplementary:
+            matches = list(pattern.finditer(read.query_sequence, overlapped=True))
 
-        found = pattern.search(read.query_sequence)
-        if not found:
-            continue
+            if not matches:
+                continue
+            if len(matches) >= 2 :
+                print 'happend'
+            print matches
+            print '\t'.join(read.to_string().split('\t'))
 
-        start_index = found.start()
-        end_index = found.end()
-        quality_sring = read.to_string().split('\t')[10][start_index:end_index]
+            for found in matches:
 
-        read.query_sequence = read.query_sequence[start_index:end_index]
+                start_index = found.start()
+                end_index = found.end()
+                print "start " + str(start_index)
+                print "end " + str(end_index)
+                if all(ref_pos is None for ref_pos in
+                       read.get_reference_positions(full_length=True)[start_index:end_index]):
+                    continue
 
-        current_ind = 0
-        in_kmer = False
-        cigarString_temp = ""
-        Kmer_len = end_index - start_index
+                quality_string = read.to_string().split('\t')[10][start_index:end_index]
+                print "qual " + quality_string
 
-        if all(ref_pos is None for ref_pos in read.get_reference_positions(full_length=True)[start_index:end_index]):
-            continue
+                kmer_read = pysam.AlignedSegment()
+                print dir(kmer_read)
+                kmer_read.query_name = read.query_name
+                kmer_read.query_sequence = read.query_sequence[start_index:end_index]
+                kmer_read.flag = read.flag
+                kmer_read.reference_id = read.reference_id
+                print kmer_read.query_name
+                print kmer_read.query_sequence
+                print kmer_read.flag
+                print read.reference_id
+                print kmer_read.reference_id
+                for ref_pos in read.get_reference_positions(full_length=True)[start_index:end_index]:
+                    if ref_pos is not None:
+                        kmer_read.reference_start = ref_pos
+                        break
 
-        for ref_pos in read.get_reference_positions(full_length=True)[start_index:end_index]:
-            if ref_pos is not None:
-                read.reference_start = ref_pos
-                break
+                print kmer_read.reference_start
+                kmer_read.mapping_quality = read.mapping_quality
+                print kmer_read.mapping_quality
 
-        for operation, count in read.cigartuples:
-            if current_ind >= end_index:
-                break
+                current_ind = 0
+                in_kmer = False
+                cigarString_temp = ""
 
-            if cigar_map[operation] == 'N' or cigar_map[operation] == 'D':
-                if in_kmer:
-                    cigarString_temp += str(count) + cigar_map[operation]
+                for operation, count in read.cigartuples:
+                    if current_ind >= end_index:
+                        break
 
-            elif cigar_map[operation] == 'M' or cigar_map[operation] == 'I' or cigar_map[operation] == 'S':
-                if current_ind + count > start_index:
-                    cigarString_temp += str(min(end_index, current_ind + count) - max(current_ind, start_index)) + \
-                                        cigar_map[operation]
-                    in_kmer = True
-                current_ind += count
-            else:
-                logging.warn('Unexpected cigar op {}'.format(cigar_map[operation]))
+                    if cigar_map[operation] == 'N' or cigar_map[operation] == 'D':
+                        if in_kmer:
+                            cigarString_temp += str(count) + cigar_map[operation]
 
-        read.cigarstring = cigarString_temp
-        read.query_qualities = pysam.qualitystring_to_array(quality_sring)
+                    elif cigar_map[operation] == 'M' or cigar_map[operation] == 'I' or cigar_map[operation] == 'S':
+                        if current_ind + count > start_index:
+                            cigarString_temp += str(
+                                min(end_index, current_ind + count) - max(current_ind, start_index)) + cigar_map[
+                                                    operation]
+                            in_kmer = True
+                        current_ind += count
+                    else:
+                        logging.warn('Unexpected cigar op {}'.format(cigar_map[operation]))
 
-        kmer_reads.write(read)
+                kmer_read.cigarstring = cigarString_temp
+                print kmer_read.cigarstring
+                kmer_read.query_qualities = pysam.qualitystring_to_array(quality_string)
+                print kmer_read.query_qualities
+
+                kmer_reads.write(kmer_read)
+                print kmer_read
+
+    kmer_reads.close()
+    samfile.close()
+    logging.info("Done!")
 
 
-kmer_reads.close()
-samfile.close()
-logging.info("Done!")
-
-
+if __name__ == '__main__':
+    main()
