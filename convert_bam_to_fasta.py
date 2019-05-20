48d690fe8ef54c4badbde22f65da6f3c1b8ddb2e
max555beyond <shengjie@email.unc.edu>
2019-05-20 13:46:16 -0400

    final version

--- convert_bam_to_fasta.py
+++ convert_bam_to_fasta.py
@@ -1,25 +1,31 @@
 import pysam
 import argparse
 
-parser = argparse.ArgumentParser(description='Utility for converting bam file to fasta files.')
-parser.add_argument('input_bam_file', type=str, nargs='?', help='provide input bam file path here')
-parser.add_argument('out_file1', type=str, nargs='?', help='provide output R1 fasta file path here')
-parser.add_argument('out_file2', type=str, nargs='?', help='provide output R2 fasta file path here')
-args = parser.parse_args()
 
-outf1 = open(args.out_file1, "w")
-outf2 = open(args.out_file2, "w")
-samfile = pysam.AlignmentFile(args.input_bam_file, "rb")
+def main():
+    parser = argparse.ArgumentParser(description='Utility for converting bam file to fasta files.')
+    parser.add_argument('input_bam_file', type=str, nargs='?', help='provide input bam file path here')
+    parser.add_argument('out_file1', type=str, nargs='?', help='provide output R1 fasta file path here')
+    parser.add_argument('out_file2', type=str, nargs='?', help='provide output R2 fasta file path here')
+    args = parser.parse_args()
 
-for read in samfile.fetch():
-    if not read.is_unmapped and not read.is_secondary and read.is_proper_pair and read.is_paired and\
-            not read.is_duplicate and not read.is_supplementary:
-        if read.is_read1:
-            outf1.write(">" + read.query_name + '\n')
-            outf1.write(read.query_alignment_sequence + '\n')
-        elif read.is_read2:
-            outf2.write(">" + read.query_name + '\n')
-            outf2.write(read.query_alignment_sequence + '\n')
+    outf1 = open(args.out_file1, "w")
+    outf2 = open(args.out_file2, "w")
+    samfile = pysam.AlignmentFile(args.input_bam_file, "rb")
 
-outf1.close()
-outf2.close()
+    for read in samfile.fetch():
+        if not read.is_unmapped and not read.is_secondary and read.is_proper_pair and read.is_paired and\
+                not read.is_duplicate and not read.is_supplementary:
+            if read.is_read1:
+                outf1.write(">" + read.query_name + '\n')
+                outf1.write(read.query_alignment_sequence + '\n')
+            elif read.is_read2:
+                outf2.write(">" + read.query_name + '\n')
+                outf2.write(read.query_alignment_sequence + '\n')
+
+    outf1.close()
+    outf2.close()
+
+
+if __name__ == '__main__':
+    main()
