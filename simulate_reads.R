933b1893c65f7bd1dd74ba7939d49526ceacc6ce
max555beyond <shengjie@email.unc.edu>
2019-05-28 01:00:48 -0400

    final version

--- simulate_reads.R
+++ simulate_reads.R
@@ -4,15 +4,15 @@ library(polyester)
 
 args = commandArgs(trailingOnly=TRUE)
 
-if (length(args)==2) {
-    args[3] = "cdnaf"
+if (length(args)==3) {
+    args[4] = "cdnaf"
 }
 
-fasta_file = "/pine/scr/s/h/shengjie/hg19.fa"
+fasta_file = args[3]
 gtfpath = args[1]
 fasta = readDNAStringSet(fasta_file)
 gtf = gffRead(gtfpath)
 
 
 simulate_experiment(gtf=gtf, seqpath=fasta, num_reps=1, fold_changes=1, outdir=args[2], readlen=100,
-                    reads_per_transcript=2000, distr="normal", error_model="illumina5", bias=args[3])
+                    reads_per_transcript=2000, distr="normal", error_model="illumina5", bias=args[4])
