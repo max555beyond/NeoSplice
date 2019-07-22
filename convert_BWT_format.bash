ce46bd31efed353d1cfcc7f4b441454995343c01
max555beyond <shengjie@email.unc.edu>
2019-07-22 16:52:21 -0400

    final version

--- convert_BWT_format.bash
+++ convert_BWT_format.bash
@@ -1,5 +1,5 @@
-input=$1
-output=$2
+input_dir=$1
+output_dir=$2
 
-cat $input/bwt_test.0.dat | tr "\000\001\002\003\004\005" "\$ACGNT" | msbwt convert $output
+cat ${input_dir}/bwt_test.0.dat | tr "\000\001\002\003\004\005" "\$ACGNT" | msbwt convert ${output_dir}
 
